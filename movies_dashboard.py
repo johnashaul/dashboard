c115a2855b7e112db3686502a3ff560d1beeef54
johnashaul <sba24271@student.cct.ie>
2025-05-21 17:46:31 +0100

    Update movies_dashboard.py

--- movies_dashboard.py
+++ movies_dashboard.py
@@ -9,14 +9,25 @@ def load_data():
     ratings = pd.read_csv('rating.csv', encoding='ISO-8859-1')
     df = ratings.merge(movies, on='movieId')
     return df
+    
+def load_recs():
+    return pd.read_csv("top30_recs.csv")  
 
 data = load_data()
+recs_df = load_recs()
 
 st.title('🎬 Movie Ratings Dashboard')
 
-# Clean Top 10 display
+
 drop_dups = data[['movieId', 'title', 'movie_avg_rating', 'ratings_count']].drop_duplicates()
 top_10_movies = drop_dups.sort_values(by='movie_avg_rating', ascending=False).head(10)
+top_10_movies['movie_avg_rating'] = top_10_movies['movie_avg_rating'].round(2)
+
+top_10_movies = top_10_movies.rename(columns={
+    'title': 'Movie Title',
+    'movie_avg_rating': 'Average Rating',
+    'ratings_count': 'No of Ratings'
+})
 
 st.subheader("Top 10 Movies by Average Rating")
 st.dataframe(top_10_movies.reset_index(drop=True))
@@ -35,15 +46,18 @@ if search_box_text:
         st.subheader('Search Results')
         st.dataframe(match_df.reset_index(drop=True))
         
+        # get the movie that most closely matches the search string
         best_match = match_list.iloc[0]
         bm_movie_id = best_match['movieId']
         bm_title = best_match['title']
         bm_genres = best_match['genres']
         bm_avg_rat = best_match['movie_avg_rating']
         
+        # get ratings for movie
         movie_ratings = found[found["movieId"] == bm_movie_id]["rating"]
         avg_rating = movie_ratings.mean()
 
+        # plot histogram of ratings for movie
         fig, ax = plt.subplots(figsize=(3, 2))
         ax.hist(movie_ratings, bins=5, edgecolor='black', color='#2c7fb8')
         ax.set_xlabel('Rating')
